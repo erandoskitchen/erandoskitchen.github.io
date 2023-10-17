da5f85cee3d533b01e47d1a39001a4fd50a0027b
erandoskitchen <148269844+erandoskitchen@users.noreply.github.com>
2023-10-18 04:13:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function() {
+    AOS.init();
+    // const ramen3 = document.querySelector(".ramen-3");
+    // const ramen4 = document.querySelector(".ramen-4");
+
+    // ramen3.style.visibility = "hidden";
+    // ramen4.style.visibility = "hidden";
+
+    // const wellcomeText = document.querySelector(".wellcome-text");
+    // wellcomeText.style.animation = "fadeIn 2s ease-in";
+
+    // setTimeout(function() {
+    //     ramen3.style.visibility = "visible";
+    //     ramen3.style.animation = "fadeIn 2s ease-in";
+    // }, 900);
+
+    // setTimeout(function() {
+    //     ramen4.style.visibility = "visible";
+    //     ramen4.style.animation = "slideRight 2s ease-in";
+    // }, 900);
+
+    // const aboutContent = document.querySelector('.about-content');
+    // const options = {
+    //     root: null,
+    //     rootMargin: '0px',
+    //     threshold: 0.5, 
+    // };
+
+    
+
+    // const observer = new IntersectionObserver((entries, observer) => {
+    //     entries.forEach((entry) => {
+    //         if (entry.isIntersecting) {
+    //             aboutContent.classList.add('show');
+    //             observer.unobserve(entry.target);
+    //         }
+    //     });
+    // }, options);
+
+    // observer.observe(aboutContent);
+});
